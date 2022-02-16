fc9807295be68b8d5f87b376d4c58a76edf210ee
Grumbleur <remi.gaiffe@intent-technologies.eu>
2022-02-16 10:13:13 +0100

    [04-structs-methods-and-interfaces]: First work

---
+++ 04-structs-methods-and-interfaces/go.mod
@@ -0,0 +1,3 @@
+module 04-structs-methods-and-interfaces
+
+go 1.17
---
+++ 04-structs-methods-and-interfaces/shapes.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
